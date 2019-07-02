931bb4b091071d28adb89cfae0a233817c0542fc
d353163 <damitha.n.gunawardena@team.telstra.com>
2019-07-02 12:17:06 +1000

    first commit

---
+++ thermometer.ino
@@ -0,0 +1,54 @@
+#include <math.h>
+#include <IRremote.h>
+
+IRsend irsend;
+double maxTemp = 24.0;
+double minTemp = 18.0;
+bool heaterOn = false;
+
+double temperature() {
+  double Temp;
+  int RawADC = analogRead(0);
+  Temp = log(((10240000/RawADC) - 10000));
+  Temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * Temp * Temp ))* Temp );
+  Temp = Temp - 273.15; // Convert Kelvin to Celcius
+  Serial.println(Temp);
+  return Temp;
+}
+
+void turnOn() {
+   irsend.sendNEC(0x00ff807f, 32);
+   delay(2000);
+   //FF906F
+   irsend.sendNEC(0x00ff906f, 32);
+}
+
+void turnOff() {
+   irsend.sendNEC(0x00ff807f, 32);
+}
+
+void setup() {
+   Serial.begin(9600);
+}
+void loop()
+{ 
+  double temp = temperature();
+  if ( temp > minTemp && temp < maxTemp) {
+    Serial.println("Current Temp " );
+    Serial.println(temp);
+    Serial.println("Doing Nothing");
+  } else if ( ( temp  < minTemp ) && !heaterOn){
+    Serial.println("Current Temp ");
+    Serial.println(temp);
+    Serial.println("Turning on the heater");
+    heaterOn = true;
+    turnOn();
+  } else if ( ( temp > maxTemp ) && heaterOn) {
+    Serial.println("Current Temp ");
+    Serial.println(temp);
+    Serial.println("Turning off the heater");
+    heaterOn = false;
+    turnOff();
+  }
+  delay(60000);
+}
